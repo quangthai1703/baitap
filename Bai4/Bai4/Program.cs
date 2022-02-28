7314f1f9e6ed159dd0947b15a78728d15de19b34
quanthai <phanquangthai1703@mail.com>
2022-02-28 23:35:35 +0700

    first commit

---
+++ Bai4/Bai4/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Bai4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Vehicle xe1 = new Vehicle("Nguyen Thu Loan","Future Neo", 35000000, 100);
+            Vehicle xe2 = new Vehicle("Le Minh Tinh", "Ford Ranger", 250000000,3000);
+            Vehicle xe3 = new Vehicle("Nguyen Minh Triet", "Landscape",1000000000,1500);
+            xe1.TinhThue();
+            xe2.TinhThue(); 
+            xe3.TinhThue();
+            Console.WriteLine("Ten Chu Xe          Loai xe          Dung tich          Tri gia          Thue phai nop  ");
+            Console.WriteLine("=========================================================================================");
+            Console.WriteLine(xe1.ToString());
+            Console.WriteLine(xe2.ToString());
+            Console.WriteLine(xe3.ToString());
+        }
+    }
+}
---
+++ Bai4/Bai4/Vehicle.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bai4
+{
+    internal class Vehicle
+    {
+        string ChuXe;
+        string LoaiXe;
+        int TriGia;
+        int DungTich;
+        int Thue;
+       public void setChuXe(string ChuXe)
+        {
+            this.ChuXe=ChuXe;
+        }
+        public void setLoaiXe(string LoaiXe)
+        {
+            this.LoaiXe = LoaiXe;
+        }
+        public void setTriGia(int TriGia)
+        {
+            if (TriGia < 0)
+                this.TriGia = 0;
+            this.TriGia= TriGia;
+        }
+        public void setDungTich(int DungTich)
+        {
+            if (DungTich< 0)
+                this.DungTich = 0;
+            this.DungTich = DungTich;
+        }
+        public string getChuXe()
+        {
+            return ChuXe;
+        }
+        public string getLoaiXe() { return LoaiXe; }
+        public int getTriGia() { return TriGia; }
+        public int getDungTich() { return DungTich; }
+        public void TinhThue()
+        {
+            if (DungTich < 100)
+            { Thue = TriGia / 100; }
+            else if (DungTich < 200)
+            {
+                Thue = (TriGia / 100)*3;
+            }
+            else
+            {
+              Thue = (TriGia /100)*5;
+            }
+        }
+        public int getThue() { return Thue; }
+        public Vehicle(string chuXe, string loaiXe, int triGia, int dungTich)
+        {
+          setChuXe(chuXe);
+          setDungTich(dungTich);
+          setLoaiXe(loaiXe);
+          setTriGia(triGia); 
+        }
+
+        public Vehicle()
+        {
+        }
+
+        public override string ToString()
+        {
+            return string.Format("{0,-20} {1,-10} {2,10} {3,22:n} {4,22:n}", getChuXe(), getLoaiXe(), getDungTich(), getTriGia(), getThue());
+        }
+    }
+}
